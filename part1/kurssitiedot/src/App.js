69c0af28ff89bde7d64a8e87a232f08a98716e49
iioskort <iiro.o.korte@student.jyu.fi>
2022-09-02 20:36:19 +0300

    part 1.5 kohassa

---
+++ part1/kurssitiedot/src/App.js
@@ -0,0 +1,57 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ course: { parts } }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ course: { parts } }) => {
+  let sum = 0;
+  parts.forEach((element) => (sum += element.exercises));
+  return <p> Number of exercises {sum} </p>;
+};
+
+export default App;
